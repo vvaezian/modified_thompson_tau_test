04a30615372deff127544b4686dfa3e806692456
Vahid <vahid.vaezian@gmail.com>
2021-09-27 22:32:51 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -86,25 +86,23 @@ def run_modified_thompson_tau_test(data, target_column_index=None, target_column
       # assuming the list is one-dimentional
       target_column_index = 0
     df = pd.Dataframe(data)
-      
-  target_column = df[df.columns[target_column_index]]
   
   # adding is_outlier and outlier_desc columns
   is_outlier = [0] * len(df) 
   outlier_col = pd.DataFrame({'is_outlier':is_outlier})
   df_with_outlier_col = pd.concat([df, outlier_col], ignore_index=True, axis=1)
-  df_with_outlier_col = df_with_outlier_col if sorted else df_with_outlier_col.sort_values(by=(df_with_outlier_col.columns[target_column_index]), inplace=True)
+  df_with_outlier_col_sorted = df_with_outlier_col if sorted else df_with_outlier_col.sort_values(by=(df_with_outlier_col.columns[target_column_index]), inplace=True)
+  target_column_sorted = df_with_outlier_col_sorted[df_with_outlier_col_sorted.columns[target_column_index]]
   
   while True:
-    #modified_series = pd.Series(sorted(list(set(series)), reverse=True))
-    res, position = run_alg_one_step(sorted_series, strict=strict)
+    res, position = run_alg_one_step(target_column_sorted, strict=strict)
     if res == 0:  # The current records is not an outlier, so the rest of the records are not outlier either.
       break
     else: # The current records is an outlier. Remove it and run the test on the rest of the dataset
       if position == 0:
-        df_with_outlier_col.iloc[sorted_series.index[0], df_with_outlier_col.columns.get_loc('is_outlier_v')] = 1
-        sorted_series = sorted_series.iloc[1:]
+        df_with_outlier_col_sorted.iloc[target_column_sorted.index[0], df_with_outlier_col_sorted.columns.get_loc('is_outlier_v')] = 1
+        target_column_sorted = target_column_sorted.iloc[1:]
       else:
-        df_with_outlier_col.iloc[sorted_series.index[-1], df_with_outlier_col.columns.get_loc('is_outlier_v')] = 1
-        sorted_series = sorted_series.iloc[:-1]
+        df_with_outlier_col_sorted.iloc[target_column_sorted.index[-1], df_with_outlier_col_sorted.columns.get_loc('is_outlier_v')] = 1
+        target_column_sorted = target_column_sorted.iloc[:-1]
   
